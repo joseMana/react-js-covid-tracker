6c609f52a5811cc5332efda2827c666f8efa502a
Seph Manangan <JosephM@magenic.com>
2021-01-20 19:23:05 +0800

    - Implemented 'Select/Unselect' logic with change of button color

--- src/components/CountryEntryComponent.tsx
+++ src/components/CountryEntryComponent.tsx
@@ -1,18 +1,32 @@
 import Country from "../models/Country"
 import { TableRow, TableCell } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { updateCommaList } from "typescript";
+import { red } from "@material-ui/core/colors";
 
 type Props = {
     country: Country,
 }
 
+
+
 const CountryEntry: React.FC<Props> = (props) => {
     const { country } = props;
+    let btnClassName = 'btn btn-primary';
+
+    const [updatedText, setText] = useState(props.country.currentText);
+    const [updatedClassName, setClassName] = useState(btnClassName);
+    function OnClickSetText() {
+        setText((updatedText === 'Select' ? 'Unselect' : 'Select'));
+
+        btnClassName = (updatedText === 'Select' ? 'btn btn-danger' : 'btn btn-primary');
+        setClassName(btnClassName);
+    }
 
     return <TableRow key={country.countryCode}>
         <TableCell align="center">
-            <button>
-                Select
+            <button className={updatedClassName} onClick={OnClickSetText}>
+                {updatedText}
             </button>
         </TableCell>
         <TableCell>{country.name}</TableCell>
